d97ba9d3a3e73c3436dced66d32d6bbc956ff41f
yunfangguo-ext-aa <yunfangguo-ext@appannie.com>
2017-05-14 15:38:24 +0800

    able to click once and merge

---
+++ execute.js
@@ -0,0 +1,5 @@
+var btn = document.getElementsByClassName("btn btn-primary BtnGroup-item js-details-target")[1];
+btn.click();
+
+var merge = document.getElementsByClassName("btn btn-primary BtnGroup-item js-merge-commit-button")[0];
+merge.click();
