cb9000e6e35b69faf3bd4d330329565bca93e035
yunfangguo-ext-aa <yunfangguo-ext@appannie.com>
2017-05-14 16:37:51 +0800

    not finished..

--- click.js
+++ click.js
@@ -1,7 +1,5 @@
-// Copyright (c) 2017 Robin. All rights reserved.
-
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-	alert("Merging Code...");
+	// alert("Merging Code...");
 	chrome.tabs.executeScript(null, {file: "/execute.js"});
 });
--- execute.js
+++ execute.js
@@ -1,9 +1,35 @@
-var btn = document.getElementsByClassName("btn btn-primary BtnGroup-item js-details-target")[1];
-btn.click();
+// var btn = document.getElementsByClassName("btn btn-primary BtnGroup-item js-details-target")[1];
+// btn.click();
 
-var merge = document.getElementsByClassName("btn btn-primary BtnGroup-item js-merge-commit-button")[0];
-merge.click();
+// var merge = document.getElementsByClassName("btn btn-primary BtnGroup-item js-merge-commit-button")[0];
+// merge.click();
 
 //
 // click -> show comment zone -> choose Approve -> click submit -> wait entil page loading done -> at conversation page, click squash and merge. 
 //
+
+var comment_array = [
+	"good code",
+	"well done",
+	"something really amazing",
+	"no comments"
+];
+
+// Review changes button click
+document.getElementsByClassName("btn btn-sm btn-primary js-menu-target")[0].click(); 
+// leave a comment by random
+document.getElementsByName("pull_request_review[body]")[0].value = comment_array[Math.floor((Math.random() * 4) )];
+// choose Approve radio button 
+document.getElementsByName("pull_request_review[event]")[1].click();
+// click submit button
+var f = document.getElementsByClassName('pull-request-review-menu dropdown-menu dropdown-menu-sw p-0');
+f.getElementsByTagName('button').click();
+
+// wait until page loading done.
+var url = window.location.href 
+var reg = '/^\d+$/';
+if (reg.test(url) != true ){
+	// at conversation page, squash and merge
+	document.getElementsByClassName("btn btn-primary BtnGroup-item js-details-target")[1].click;
+	document.getElementsByClassName("btn btn-primary BtnGroup-item js-merge-commit-button").click;	
+}
